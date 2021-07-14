5cdbad2a90c3dc8c12e044b9bdf7d51ecb2438f3
Al Hussain <ahussain@winsystems.com>
2021-07-14 19:04:04 +0000

    Updated dictionary for support database with new 'time_start' field.

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import inquirer
 import glob
 import subprocess
-from datetime import date
+import datetime
 import sys
 import os
 import pyodbc
@@ -21,7 +21,7 @@ print("--Requires an image file and bmap file created using bmap-tools")
 print("--files must be in /home/winsys/ directory\n\n")
 
 print(Style.RESET_ALL)
-date = date.today()
+date = datetime.datetime.now()
 print(date)
 
 while True:
@@ -136,11 +136,11 @@ try:
 ############################################################################################
 
 
-	supdict = {'ws_serial_num':'','Sup_Description':'','Sup_Value':'','Test_Date':''}
+	supdict = {'ws_serial_num':'','Sup_Description':'','Sup_Value':'','time_start':datetime.datetime(1,1,1)}
 	supdict ['ws_serial_num'] = sn
 	supdict['Sup_Description'] = 'Linux Image'
 	supdict['Sup_Value'] = answer['imgfile'].split('/')[-1]
-	supdict['Test_Date'] = date.strftime('%-m/%-d/%Y')
+	supdict['time_start'] = date
 
 #	print(supdict)
 
@@ -148,10 +148,10 @@ try:
 	columns = ', '.join(supdict.keys())
 	sql = format("INSERT INTO "+"dbo.WinSys_Test_Support_Data"+" ( "+columns+" ) VALUES ( "+placeholders+" )")
 	print('\nUpdating ARAS database...\n')
-#	conn = pyodbc.connect('DSN=server;Database=TestSuite;UID=TestDevTemp;PWD=TempPass1!;Trusted_Connection=No; Connection Timeout=10')
-#	cursor = conn.cursor()
-#	cursor.execute(sql,*supdict.values())
-#	conn.commit()
+	conn = pyodbc.connect('DSN=server;Database=TestSuite;UID=TestDevTemp;PWD=TempPass1!;Trusted_Connection=No; Connection Timeout=10')
+	cursor = conn.cursor()
+	cursor.execute(sql,*supdict.values())
+	conn.commit()
 
 ######## Present prompt if no exceptions are thrown #######################################
 ###########################################################################################
